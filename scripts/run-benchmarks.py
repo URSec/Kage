adeea6fe9b93d648cbdf7729aafd1166a32aefcf
Yufei Du <edwarddu1996@gmail.com>
2022-01-19 18:21:43 -0500

    Fixed compilation script using eclipse

--- scripts/run-benchmarks.py
+++ scripts/run-benchmarks.py
@@ -3,6 +3,7 @@
 import argparse
 from pathlib import Path
 import subprocess
+from sys import stderr
 
 PROJECTS = {'microbenchmark':{'baseline':'freertos_microbenchmarks_clang', 
                    'baseline_mpu':'freertos_mpu_microbenchmarks_clang',
@@ -14,7 +15,7 @@ DEVICE = 'demos/st/stm32l475_discovery/ac6'
 OCD_CMD = 'program $PATH$ reset exit'
 BUILD_CMD = \
     '-nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild'\
-    + ' -data $WORKSPACE$ -cleanBuild $PROJECT$'
+    + ' -data $WORKSPACE$ -import $PROJPATH$ -cleanBuild $PROJECT$'
 
 # Main routine
 if __name__ == "__main__":
@@ -35,8 +36,16 @@ if __name__ == "__main__":
     # Optional subset of benchmark programs
     parser.add_argument('--programs', type=str, nargs='+', \
         default=['microbenchmark', 'coremark'])
+    # Print the output of subprocesses
+    parser.add_argument('--verbose', action='store_true', default=False)
     # Get arguments
     args = parser.parse_args()
+
+    # Set destinations of stdout and stderr according to argument
+    if args.verbose:
+        stdDst = subprocess.STDOUT
+    else:
+        stdDst = subprocess.DEVNULL
 
     # Generate project paths
     for program in args.programs:
@@ -50,24 +59,26 @@ if __name__ == "__main__":
                 exit()
             projectPath = Path(args.workspace).joinpath(projProgram[config])\
                 .joinpath(DEVICE)
+            # Import the project to System Workbench's workspace
+            # and build the binaries
+            ac6Arg = BUILD_CMD.replace('$WORKSPACE$', \
+                    args.workspace.as_posix())
+            ac6Arg = ac6Arg.replace('$PROJPATH$', projectPath.as_posix())
+            ac6Arg = ac6Arg.replace('$PROJECT$', projProgram[config])
+            ac6Arg = args.ac6.as_posix() + ' ' + ac6Arg
+            subprocess.run(ac6Arg, shell=True, capture_output=False,
+                           stderr=stdDst, stdout=stdDst, check=True)
             for configDir in projectPath.iterdir():
                 if not configDir.is_dir():
                     continue
                 if configDir.name[0] == '.':
                     continue
                 print('Running ', program, ' ', config, ' ', configDir.name)
-                # Build the binary using System WorkBench
-                projName = projProgram[config] + '/' + configDir.name
-                ac6Arg = BUILD_CMD.replace('$WORKSPACE$', \
-                    args.workspace.as_posix()).replace('$PROJECT$', projName)
-                ac6Arg = args.ac6.as_posix() + ' ' + ac6Arg
-                subprocess.run(ac6Arg, shell=True)
-                print('Compilation finished')
                 # Execute OpenOCD on binary found in each build config
                 binPath = configDir.joinpath(configDir.name + '.elf')
                 ocdArg = OCD_CMD.replace('$PATH$', binPath.as_posix())
                 subprocess.run([args.openocd, '-f', args.ocdcfg.as_posix(), '-c', \
-                    ocdArg])
+                    ocdArg], stderr=stdDst, stdout=stdDst, check=True)
                 print('Finished ', binPath.name)
                 # Pause until user input
                 input('Press Enter to continue')
