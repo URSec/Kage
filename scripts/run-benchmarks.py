ad001c307f2a193da4a89a575a008a83e4e7f95e
Yufei Du <edwarddu1996@gmail.com>
2022-01-25 22:12:55 -0500

    [Untested] Added built-in serial parser

--- scripts/run-benchmarks.py
+++ scripts/run-benchmarks.py
@@ -4,6 +4,7 @@ import argparse
 from colorama import Back, Fore, Style
 from pathlib import Path
 from elftools.elf.elffile import ELFFile
+import serial
 import subprocess
 from sys import stderr
 
@@ -81,59 +82,71 @@ if __name__ == "__main__":
         stdDst = subprocess.STDOUT
     else:
         stdDst = subprocess.DEVNULL
-
-    # Generate project paths
-    for program in args.programs:
-        if not program in PROJECTS:
-            print('ERROR: Unknown program ', program)
-            exit()
-        if args.disable_cache and not program == 'coremark':
-            print('WARNING: --disable_cache option only supports coremark. Skipping ', program)
-            continue
-        projProgram = PROJECTS[program]
-        for config in args.configs:
-            if not config in projProgram:
-                print('ERROR: Unknown configuration ', config)
+    
+    # Initialize dict to store results
+    perfDict = {}
+    sizeDict = {}
+    
+    # Initialize serial interface with 1 minute of timeout
+    with serial.Serial('/dev/ttyACM0', 115200, timeout=60) as ser:
+        # Generate project paths
+        for program in args.programs:
+            if not program in PROJECTS:
+                print('ERROR: Unknown program ', program)
                 exit()
-            if projProgram[config] == '':
-                # No benchmark for this config and program combination,
-                # skipping
+            if args.disable_cache and not program == 'coremark':
+                print('WARNING: --disable_cache option only supports coremark. Skipping ', program)
                 continue
-            projectPath = Path(args.workspace).joinpath(projProgram[config])\
-                .joinpath(DEVICE)
-            # Import the project to System Workbench's workspace
-            # and build the binaries
-            print('Compiling ', program, ' for ', config, '...')
-            ac6Arg = BUILD_CMD.replace('$WORKSPACE$', \
-                    args.workspace.as_posix())
-            ac6Arg = ac6Arg.replace('$PROJPATH$', projectPath.as_posix())
-            ac6Arg = ac6Arg.replace('$PROJECT$', projProgram[config])
-            ac6Arg = args.ac6.as_posix() + ' ' + ac6Arg
-            subprocess.run(ac6Arg, shell=True, capture_output=False,
-                           stderr=stdDst, stdout=stdDst, check=True)
-            for configDir in projectPath.iterdir():
-                if not configDir.is_dir():
+            # Initialize dict to store results
+            perfDict[program] = {}
+            sizeDict[program] = {}
+
+            projProgram = PROJECTS[program]
+            for config in args.configs:
+                if not config in projProgram:
+                    print('ERROR: Unknown configuration ', config)
+                    exit()
+                if projProgram[config] == '':
+                    # No benchmark for this config and program combination,
+                    # skipping
                     continue
-                if configDir.name[0] == '.':
-                    continue
-                # Only run configs without cache when requested.
-                # Also, when requested to run without cache, skip configs
-                # with cache. 
-                if ('no-cache' in configDir.name) != (args.disable_cache):
-                    continue
-                print(f'Flashing {Fore.GREEN}', program, ' ',
-                    translateConfigName(configDir.name), f'{Style.RESET_ALL}')
-                # Execute OpenOCD on binary found in each build config
-                binPath = configDir.joinpath(configDir.name + '.elf')
-                ocdArg = OCD_CMD.replace('$PATH$', binPath.as_posix())
-                subprocess.run([args.openocd, '-f', args.ocdcfg.as_posix(), '-c', \
-                    ocdArg], stderr=stdDst, stdout=stdDst, check=True)
-                print('Finished flashing ', binPath.name, ' to the board.')
-                print('Please check the serial terminal for results.')
-                print('Some benchmark programs may take up to 30 seconds to show results.')
-                # Print code size if requested
-                if not args.no_code_size:
-                    print(f'{Back.CYAN}{Fore.BLACK}Code size: {Style.RESET_ALL}')
+                projectPath = Path(args.workspace).joinpath(projProgram[config])\
+                    .joinpath(DEVICE)
+                # Initialize dict to store results
+                perfDict[program][config] = {}
+                sizeDict[program][config] = {}
+                # Import the project to System Workbench's workspace
+                # and build the binaries
+                print('Compiling ', program, ' for ', config, '...')
+                ac6Arg = BUILD_CMD.replace('$WORKSPACE$', \
+                        args.workspace.as_posix())
+                ac6Arg = ac6Arg.replace('$PROJPATH$', projectPath.as_posix())
+                ac6Arg = ac6Arg.replace('$PROJECT$', projProgram[config])
+                ac6Arg = args.ac6.as_posix() + ' ' + ac6Arg
+                subprocess.run(ac6Arg, shell=True, capture_output=False,
+                            stderr=stdDst, stdout=stdDst, check=True)
+                for configDir in projectPath.iterdir():
+                    if not configDir.is_dir():
+                        continue
+                    if configDir.name[0] == '.':
+                        continue
+                    # Only run configs without cache when requested.
+                    # Also, when requested to run without cache, skip configs
+                    # with cache. 
+                    if ('no-cache' in configDir.name) != (args.disable_cache):
+                        continue
+                    print(f'Flashing {Fore.GREEN}', program, ' ',
+                        translateConfigName(configDir.name), f'{Style.RESET_ALL}')
+                    # Execute OpenOCD on binary found in each build config
+                    binPath = configDir.joinpath(configDir.name + '.elf')
+                    ocdArg = OCD_CMD.replace('$PATH$', binPath.as_posix())
+                    # Flash the binary asynchronously to immediate receive serial
+                    # output
+                    subprocess.run([args.openocd, '-f', args.ocdcfg.as_posix(), '-c', \
+                        ocdArg], stderr=stdDst, stdout=stdDst)
+                    # Determine the human-readable configuration name
+                    confName = translateConfigName(configDir.name)
+                    # Compute code size
                     with binPath.open('rb') as file:
                         elffile = ELFFile(file)
                         section = elffile.get_section_by_name(\
@@ -164,8 +177,71 @@ if __name__ == "__main__":
                         else:
                             trusted = privSize
                             untrusted = syscallSize + textSize
-                        print('Trusted: ', trusted)
-                        print('Untrusted: ', untrusted)
-
-                # Pause until user input
-                input('Press Enter to flash the next benchmark.')
+                    # Read output
+                    while True:
+                        # Read output
+                        line = ser.readline()
+                        if len(line) == 0:
+                            # timeout
+                            print("ERROR: Timeout waiting for results from the board.")
+                            exit(1)
+                        # Each benchmark has different output format, so do a
+                        # manual matching here.
+                        if 'stream-buffer' in configDir.name:
+                            # Stream buffer microbenchmark
+                            if 'Creating stream buffer' in line:
+                                # Stream buffer creation
+                                t = int(line.split(': ')[1].replace('\n', '')\
+                                    .replace('\r', ''))
+                                perfDict[program][config][confName + ': create'] = t
+                            if 'Received unsigned 9 from stream buffer' in line:
+                                # Stream buffer send and receive
+                                t = int(line.split(': ')[1].replace('\n', '')\
+                                    .replace('\r', ''))
+                                perfDict[program][config][confName + ': send and receive'] = t
+                                sizeDict[program][config][confName] = \
+                                    {'trusted':trusted, 'untrusted':untrusted}
+                                break
+                        if 'queue' in configDir.name:
+                            # Queue microbenchmark
+                            if 'Creating queue' in line:
+                                # Queue creation
+                                t = int(line.split(': ')[1].replace('\n', '')\
+                                    .replace('\r', ''))
+                                perfDict[program][config][confName + ': create'] = t
+                            if 'Received unsigned 9 from queue' in line:
+                                # Queue send and receive
+                                t = int(line.split(': ')[1].replace('\n', '')\
+                                    .replace('\r', ''))
+                                perfDict[program][config][confName + ': send and receive'] = t
+                                sizeDict[program][config][confName] = \
+                                    {'trusted':trusted, 'untrusted':untrusted}
+                                break
+                        if 'exception-dispatcher' in configDir.name:
+                            # Exception microbenchmark
+                            if 'DIV_BY_0' in line:
+                                t = int(line.split(': 0 ')[1].split(' cycles')[0])
+                                perfDict[program][config][confName.replace('dispatcher', '')] = t
+                                sizeDict[program][config][confName.replace('dispatcher', '')] = \
+                                    {'trusted':trusted, 'untrusted':untrusted}
+                                break
+                        if 'context-switch' in configDir.name:
+                            # Context switch microbenchmark
+                            if 'Context Switch cycle' in line:
+                                t = int(line.split(': ')[1].split(' cycles')[0])
+                                perfDict[program][config][confName] = t
+                                sizeDict[program][config][confName] = \
+                                    {'trusted':trusted, 'untrusted':untrusted}
+                                break
+                        if 'coremark' in configDir.name:
+                            # CoreMark
+                            if 'Iterations/Sec' in line:
+                                t = float(line.split(': ')[1].replace('\n', ''))
+                                perfDict[program][config][confName] = t
+                                sizeDict[program][config][confName] = \
+                                    {'trusted':trusted, 'untrusted':untrusted}
+                                break
+        print("Performance results:")
+        print(perfDict)
+        print("Cod size results:")
+        print(sizeDict)
